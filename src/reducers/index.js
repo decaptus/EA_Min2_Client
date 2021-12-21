179fce7788173bb932ae0a2bf848e5dd3770fe92
decaptus <72303216+decaptus@users.noreply.github.com>
2021-12-21 17:55:07 +0100

    last commit

--- src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-
-import posts from './posts';
-import questions from './questions';
-import auth from './auth';
-
-export const reducers = combineReducers({ posts, questions, auth});
